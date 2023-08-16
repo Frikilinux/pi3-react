08209e940bffed0bb2d021d6e630c1683b68e0ad
Frikilinux <frikilinux@gmail.com>
2023-08-15 22:54:42 -0300

    Fix horizontal scroll

--- src/components/UI/MainWrapper/MainWrapperStd.jsx
+++ src/components/UI/MainWrapper/MainWrapperStd.jsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components'
 
 export const MainWrapper = styled.main`
+  width: 100%;
   position: relative;
   margin-top: 50px;
   display: flex;
--- src/components/UI/SectionWrapper/SectionWrapperStd.jsx
+++ src/components/UI/SectionWrapper/SectionWrapperStd.jsx
@@ -4,7 +4,7 @@ export const SectionContainer = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100vw;
+  width: 100%;
   background: url(${({ img }) => `${img}`}) center / cover no-repeat;
   background-color: ${({ bg }) => bg ?? 'var(--dark)'};
 `

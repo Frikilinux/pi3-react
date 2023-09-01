837f9245cbb9c3d5d0076b9b0877e8b1cc089cd3
Frikilinux <frikilinux@gmail.com>
2023-09-01 19:25:34 -0300

    Another test

--- src/constants/apiUrls.js
+++ src/constants/apiUrls.js
@@ -1,5 +1,5 @@
 export const NUCBAZ_API = {
-  API_PROXY: process.env.REACT_APP_API_PROXY,
+  API_PROXY: import.meta.env.REACT_APP_API_PROXY,
   ROOT: 'https://nucbaz-api.vercel.app',
   REGISTER: '/auth/register',
   LOGIN: '/auth/login',
@@ -8,7 +8,7 @@ export const NUCBAZ_API = {
 
 export const DEEZER_API = {
   // API_PROXY: 'https://cors.systec.ar:10300/', //
-  API_PROXY: process.env.VITE_API_PROXY,
+  API_PROXY: import.meta.env.VITE_API_PROXY,
   ROOT: 'https://api.deezer.com',
   GENRES: '/genre',
   ALBUM: '/album',
